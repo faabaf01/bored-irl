7c23502b42f0153c9443dc5590ad2040a39e40e8
faabaf01 <farasya15@gmail.com>
2026-03-29 00:56:17 +0800

    add coordinate pin points

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -4,22 +4,31 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'; // Import compatibility CSS
 import 'leaflet-defaulticon-compatibility'; // Import compatibility JS
+import { LOCATIONS } from "@/lib/locations";
 
 const MapComponent = () => {
     const position: [number, number] = [37.5665, 126.978];
+    
     return (
-        <MapContainer center={position} zoom={10} style={{ height: '400px', width: '100%'}}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
-                <Popup>
-                    {position[0]}, {position[1]}
-                </Popup>
-            </Marker>
-        </MapContainer>
-    )
+      <MapContainer
+        center={position}
+        zoom={10}
+        style={{ height: "400px", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {LOCATIONS.map((location) => (
+          <Marker
+            key={location.name}
+            position={[location.latitude, location.longitude]}
+          >
+            <Popup>{location.name}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    );
     };
 
 export default MapComponent;
